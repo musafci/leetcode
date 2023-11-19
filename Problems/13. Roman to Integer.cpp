411bef4c7d911fb3669436a52adfba5da35ef803
Md Abu Musa <33667364+musafci@users.noreply.github.com>
2023-11-20 00:58:42 +0600

    Update 13-Roman to Integer

--- Problems/13. Roman to Integer.cpp
+++ Problems/13. Roman to Integer.cpp
@@ -1,29 +1,22 @@
 // Problem: https://leetcode.com/problems/roman-to-integer/description/
 
 /**
- * Time Complexity:
- * Space Complexity:
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
 */
 
 class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char, int> rti = {{'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}};
-        unordered_map<int, char> roman;
 
-        for (int i = 0; i < s.length(); i++) {
-            roman[i] = s[i];
-        }
-        // for (auto jk : roman) {
-        //     cout << jk.first << " " << jk.second << endl;
-        // }
         int result = 0;
-        for (int j = 0; j < s.length(); j++) {
-            int current_value = s[j];
-            if (current_value ) {
-
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && rti[s[i]] < rti[s[i + 1]]) {
+                result -= rti[s[i]];
+            } else {
+                result += rti[s[i]];
             }
-            result = result + rti[it];
         }
         return result;
     }
